1bb6b3782c0836d0a772c25b0d5beb164d92f489
Waliston <goneisgone@hotmail.com>
2023-07-07 20:31:09 -0300

    Web Cursos - Update

--- frontend/src/components/Nav/Nav.Component.jsx
+++ frontend/src/components/Nav/Nav.Component.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import "./nav.css";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
-    return (
-        <div className="nav-compnent">
-            <div className="nav--itens">
-                <h2>master lern</h2>
-                <div className="log--itens">
-                    <li>login</li>
-                    <li>cadastrar</li>
-                </div>
-            </div>
+  return (
+    <div className="nav-compnent">
+      <div className="nav--itens">
+        <h2>master lern</h2>
+        <div className="log--itens">
+          <Link to="" className="link__to">entrar</Link>
+          <Link to="" className="link__to">cadastrar</Link>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Nav;
