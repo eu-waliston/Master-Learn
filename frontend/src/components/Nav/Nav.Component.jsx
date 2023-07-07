a7c630f487608ce436ad8d625fc06b726a984dca
Waliston <goneisgone@hotmail.com>
2023-07-07 20:43:29 -0300

    Web Cursos - Navbar 100%

--- frontend/src/components/Nav/Nav.Component.jsx
+++ frontend/src/components/Nav/Nav.Component.jsx
@@ -6,9 +6,18 @@ const Nav = () => {
   return (
     <div className="nav-compnent">
       <div className="nav--itens">
-        <h2>master lern</h2>
+        <div>
+            <h2>master lern</h2>
+        </div>
+        <div className="web__iconst">
+            <a href="#home">inicio</a>
+            <a href="#about-us">sobre nós</a>
+            <a href="#tutors">professores</a>
+            <a href="#depoiments">depoimentos</a>
+            <a href="#contact-us">contato</a>
+        </div>
         <div className="log--itens">
-          <Link to="" className="link__to">entrar</Link>
+          <Link to="" className="link__to">sou aluno</Link>
           <Link to="" className="link__to">cadastrar</Link>
         </div>
       </div>
